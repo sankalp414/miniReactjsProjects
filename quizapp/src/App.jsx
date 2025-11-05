7cb23320b0257f5d849127681b2c1640e6a0b0c4
Sankalp Sharma <sankalpshrm1111@gmail.com>
2025-11-05 23:31:07 +0530

    quizapp

---
+++ quizapp/src/App.jsx
@@ -0,0 +1,13 @@
+import Quiz from "./components/Quiz/Quiz"
+
+function App() {
+
+
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
---
+++ quizapp/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question:"which devce is required for the internet connection",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans:1
+    },
+    {
+        question:"which continents has the highest number of countries ",
+        option1:"Asia",
+        option2:"Europe",
+        option3:"North America",
+        option4:"Africa",
+        ans:4
+    },
+    {
+        question:"Junk e-mail is also called?",
+        option1:"Spam",
+        option2:"Fake",
+        option3:"Archived",
+        option4:"Bin",
+        ans:1
+    },
+    {
+        question:"A computer cannot boot if it does not have the ?",
+        option1:"Aplication Software",
+        option2:"Internet",
+        option3:"Operating System",
+        option4:"Mouse",
+        ans:3
+    },
+    {
+        question:"First page of website is termed as?",
+        option1:"Index Page",
+        option2:"HomePage",
+        option3:"Site map",
+        option4:"Pen Drive",
+        ans:2
+    },
+]
