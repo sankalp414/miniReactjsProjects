f52d384d54dd8b99893b6f1559d195d5997c2b4a
Sankalp Sharma <sankalpshrm1111@gmail.com>
2025-11-25 07:47:29 +0530

    memorygame

--- memorygame/src/components/Memory.jsx
+++ memorygame/src/components/Memory.jsx
@@ -35,24 +35,55 @@ const Memory = () => {
         initialiseGame()
     },[gridSize])
     
+    const checkMatch = (secondId) => {
+        const [firstId] = flipped;
+        if(cards[firstId].number === cards[secondId].number){
+          setSolved([...solved,firstId,secondId])
+          setFlipped([])
+          setDisabled(false)
+        }else{
+          setTimeout(()=>{
+            setFlipped([])
+            setDisabled(false)
+          },1000)
+        }
+
+    }
+
+
     const  handleClick = (id)=>{
       if(disabled || won) return
       if(flipped.length === 0){
         setFlipped([id])
         return
       }
+      if(flipped.length === 1){
+        setDisabled(true)
+        if(id !== flipped[0]){
+          setFlipped([...flipped,id])
+          checkMatch(id)
+        }else{
+          setFlipped([])
+          setDisabled(false)
+        }
+      }
     }
     
-    
+    const isFlipped = (id)=> flipped.includes(id) || solved.includes(id)
+    const isSolved = (id)=>solved.includes(id)
 
-
+    useEffect(()=>{
+      if(solved.length === cards.length && cards.length>0){
+        setWon(true)
+      }
+    },[solved,cards])
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen bg-grey-100 p-4'>
         <h1 className='text-3xl font-bold mb-6'>Memory Game</h1>
       {/* input */}
-        <div>
-            <label htmlFor="gridSize"> Grid Size : (max: 10) </label>
+        <div className='mb-4'>
+            <label htmlFor="gridSize" className='mr-2'> Grid Size : (max: 10) </label>
             <input 
                 type='number' 
                 id='gridSize' 
@@ -76,9 +107,12 @@ const Memory = () => {
 
             {cards.map((card)=>{
                 return <div key={card.id } 
-                className='aspect-square flex items-center justify-center text-xl font-bold rounded-lg cursor-pointer transition-all duration-300 bg-gray-300 text-gray-400'
+                className={`aspect-square flex items-center justify-center text-xl font-bold rounded-lg cursor-pointer transition-all duration-300 ${isFlipped(card.id)? isSolved(card.id)?
+                  "bg-green-500 text-white" 
+                  :"bg-blue-500 text-white"
+                  : "bg-gray-300 text-gray-400"}  `}
                 onClick={()=>handleClick(card.id)}>
-                  {card.number}
+                  {isFlipped(card.id)?card.number:"?"}
                 </div>
             })}
         </div>
@@ -86,11 +120,13 @@ const Memory = () => {
 
       {/* result */}
 
-
+      {won && <div className='mt-4 text-4xl font-bold text-green-500 animate-bounce'>You Won </div>}
 
       {/* reset button */}
 
-
+      <button onClick={initialiseGame} className='mt-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition-colors'>
+        {won?"PlayAgain":"Reset"}
+      </button>
 
     </div>
   )
